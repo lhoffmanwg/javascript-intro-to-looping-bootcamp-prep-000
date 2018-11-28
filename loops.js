44d11f4c052c5e9601cbea55905056158d769620
Laura Hoffman <lhoffmanwg@gmail.com>
2018-11-28 05:53:16 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,38 +1,24 @@
-
-
-function forLoop(params){
-  var i;
-  for (i = 0; i < 25; i++){
-      if (i === 1) {
-        params.push(`I am ${i} strange loop.`);
+function forLoop(array) {
+  for (var i=0; i < 25; i++) {
+    if (i === 1) {
+      array.push(`I am ${i} strange loop.`);
     } else {
-        params.push(`I am ${i} strange loops.`);
+      array.push(`I am ${i} strange loops.`);
     }
   }
-return params
-
+  return array;
 }
-n = 10
-function whileLoop(n){
-  while (n > 0){
-    console.log(--n);
+
+function whileLoop(n) {
+  while (n > 0) {
+    console.log(--n); 
   }
-  return 'done';
+  return "done";
 }
 
-
-
-
-function doWhileLoop(arrayOne){
-  do{ 
-    arrayOne.shift();
-    } while (arrayOne.length > 0 && incrementVariable());
-    return arrayOne;
-}
-
-var i = 0; 
-function incrementVariable() {
-
-  i = i + 1;
-  return true;
-}
+//function doWhileLoop(array) {
+//  do {
+//    array.pop;
+//  } while (array.length > 0)
+    
+//}
